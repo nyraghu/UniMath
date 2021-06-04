0098dd5c9d2e77c3d61fe39157377c75caf08e96
N. Raghavendra <raghu@hri.res.in>
2021-06-04 18:43:17 +0530

    Clean the script for generating the Coq project
    * misc/make/generate-coq-project-file.sh: Add a usage message.
      Replace some repeated code with a variable.

--- misc/make/generate-coq-project-file.sh
+++ misc/make/generate-coq-project-file.sh
@@ -8,6 +8,12 @@
 PATH=/bin:/usr/bin
 set -o errexit
 
+program="$0"
+usage="Usage: sh ${program} packages"
+if test $# -eq 0; then
+    printf "${usage}\n" >&2
+    exit 1
+fi
 packages="$@"
 
 printf -- "-Q UniMath UniMath
@@ -18,11 +24,12 @@ printf -- "-Q UniMath UniMath
 -arg -notation-overridden\n"
 
 for package in ${packages}; do
+    directory="UniMath/${package}"
     sed -e 's@#.*@@' \
         -e '/^[[:space:]]*$/d' \
-        -e "s@^[[:space:]]*@UniMath/${package}/@" \
-        "UniMath/${package}/.package/files"
-    printf "UniMath/${package}/All.v\n"
+        -e "s@^[[:space:]]*@${directory}/@" \
+        "${directory}/.package/files"
+    printf "${directory}/All.v\n"
 done
 
 printf "UniMath/All.v\n"
