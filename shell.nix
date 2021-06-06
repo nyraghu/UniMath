275b0f401db945e94d7657b8400d0a6db7110b62
N. Raghavendra <raghu@hri.res.in>
2021-06-06 22:44:44 +0530

    Stop using the `coqdoc-overlay' Nix package
    * shell.nix: Remove the code which makes the `coqdoc-overlay' package
      available in the Nix shell because I do not need foldable proofs in
      the HTML documentation of the project.

--- shell.nix
+++ shell.nix
@@ -7,20 +7,13 @@ let
       url = "https://github.com/NixOS/nixpkgs/archive/4d8dd0afd2d5a35acb5e34c5c7b1674b74173d87.tar.gz";
       sha256 = "1d7z53vzq9jjy0dmq8w952dlxvajxrbmbn9926yhfp610p93ns84";
     };
-  ixnayPath =
-    builtins.fetchTarball {
-      url = "https://github.com/nyraghu/ixnay/archive/f1dfe26543b0da17c3940e096c9953ac8895de1e.tar.gz";
-      sha256 = "02ah73mdq71clbg8jjg6y68nrlswbpcsninj39yjfyl197jp6dzl";
-    };
   nixpkgs = import nixpkgsPath {};
-  ixnay = import ixnayPath { inherit nixpkgs; };
   coq = nixpkgs.coq;
   ## This is the value of `propagatedBuildInputs' in the Nix setup of
   ## the Coq project.
   ## <https://github.com/coq/coq/blob/ac9a31046f82a4a489452b82c56a9d8cb7efc77c/default.nix#L74>
   ocamlPackages = with coq.ocamlPackages; [ findlib ocaml zarith ];
-  coqdoc-overlay = ixnay.coqdoc-overlay;
-  coqPackages = [ coq coqdoc-overlay ] ++ ocamlPackages;
+  coqPackages = [ coq ] ++ ocamlPackages;
   generalPackages = [];
   packages = coqPackages ++ generalPackages;
   manDirs = builtins.map (x: x + "/share/man") packages;
@@ -28,10 +21,7 @@ let
             + ":" + (builtins.getEnv "MANPATH");
 in nixpkgs.mkShell {
   buildInputs = packages;
-  inherit MANPATH coq;
-  ## Replace the hyphen because it is not allowed in the name of a
-  ## shell variable.
-  coqdoc_overlay = coqdoc-overlay;
+  inherit MANPATH;
 }
 
 ### End of file
